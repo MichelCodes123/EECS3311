8133c5ac120fd1963a85e7d691ab4c248f9c9edd
Philly <lfilippelli5@gmail.com>
2024-03-08 12:53:31 -0500

    Create user class

---
+++ src/Models/User.java
@@ -0,0 +1,15 @@
+package Models;
+
+import java.util.ArrayList;
+
+public interface User {
+
+    String id = null;
+    String name = null;
+    String email = null;
+    String password = null;
+    Boolean can_borrow = null;
+    Double overdue_charge = null;
+    Boolean is_registered = null;
+    ArrayList<Item> rented_item_list = null;
+}
