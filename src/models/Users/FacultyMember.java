ce14e38576debaad312c102fa9859264790c3354
Philly <lfilippelli5@gmail.com>
2024-03-10 12:24:51 -0400

    Request from Michel: User to abstract class

--- src/models/Users/FacultyMember.java
+++ src/models/Users/FacultyMember.java
@@ -4,16 +4,7 @@ import models.Items.Item;
 
 import java.util.ArrayList;
 
-public class FacultyMember implements User {
-    String id = null;
-    String name = null;
-    String email = null;
-    String password = null;
-    Boolean can_borrow = null;
-    Double overdue_charge = null;
-    Boolean is_registered = null;
-    ArrayList<String> rented_item_list = null;
-
+public class FacultyMember extends User {
     public FacultyMember(String id, String name, String email, String password, Boolean can_borrow, Double overdue_charge, Boolean is_registered, ArrayList<String> rented_item_list) {
         this.id = id;
         this.name = name;
@@ -23,37 +14,5 @@ public class FacultyMember implements User {
         this.overdue_charge = overdue_charge;
         this.is_registered = is_registered;
         this.rented_item_list = rented_item_list;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public Boolean getCan_borrow() {
-        return can_borrow;
-    }
-
-    public Double getOverdue_charge() {
-        return overdue_charge;
-    }
-
-    public Boolean getIs_registered() {
-        return is_registered;
-    }
-
-    public ArrayList<String> getRented_item_list() {
-        return rented_item_list;
     }
 }
--- src/models/Users/User.java
+++ src/models/Users/User.java
@@ -3,7 +3,7 @@ package models.Users;
 import models.Items.*;
 import java.util.ArrayList;
 
-public interface User {
+public abstract class User {
 
     String id = null;
     String name = null;
@@ -12,5 +12,38 @@ public interface User {
     Boolean can_borrow = null;
     Double overdue_charge = null;
     Boolean is_registered = null;
-    ArrayList<Item> rented_item_list = null;
+    ArrayList<String> rented_item_list = null;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Boolean getCan_borrow() {
+        return can_borrow;
+    }
+
+    public Double getOverdue_charge() {
+        return overdue_charge;
+    }
+
+    public Boolean getIs_registered() {
+        return is_registered;
+    }
+
+    public ArrayList<String> getRented_item_list() {
+        return rented_item_list;
+    }
 }
