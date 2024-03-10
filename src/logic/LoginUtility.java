c05bb3392138c7912ba1140cf51a04c55fbe726a
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-03-10 12:48:06 -0400

    Started implementing registration verification

---
+++ src/logic/LoginUtility.java
@@ -0,0 +1,53 @@
+package logic;
+
+import java.util.function.Consumer;
+import java.util.regex.Pattern;
+
+public class LoginUtility {
+
+	/**
+	 * Verifies that the user has entered a strong password.
+	 * 
+	 * @param s
+	 * @return
+	 */
+	public void verifyRegistration(String name, String email, String password, Consumer<String> errorHandler) {
+
+		name.trim();
+		email.trim();
+		password.trim();
+		if (name.length() == 0 || email.length() == 0 || password.length() == 0) {
+
+			errorHandler.accept("Cannot have empty fields!");
+			return;
+		}
+
+		if (name.length() < 4) {
+			errorHandler.accept("Password length must be greater than 4");
+			return;
+		}
+
+		Pattern p = Pattern.compile("?=.*[A-Z]");
+		if (!p.matcher(password).matches()) {
+			errorHandler.accept("Password must contain at least one uppercase letter");
+			return;
+		}
+
+//		Pattern e = Pattern.compile(".*[A-Z]@.*[A-Z].[com|ca]");
+//		if (!e.matcher(password).matches()) {
+//			errorHandler.accept("Invalid Email type");
+//			return;
+//		}
+
+		if (!verifyUniqueEmail(email)) {
+			errorHandler.accept("This email already exists");
+		}
+
+	}
+
+	public Boolean verifyUniqueEmail(String email) {
+
+		return false;
+	}
+
+}
