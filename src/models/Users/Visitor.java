75ffc726965ff1a487125b4decb45f7f1800e41b
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-03-12 11:28:14 -0400

    Refactor "Complete Rebase" Fixed issue

--- src/models/Users/Visitor.java
+++ src/models/Users/Visitor.java
@@ -8,7 +8,9 @@ import models.Items.Item;
 public class Visitor extends User {
 	public Visitor(String id, String name, String email, String password, Boolean can_borrow, Double overdue_charge,
 			Boolean is_registered, ArrayList<String> rented_item_list) {
-		super(id, name, email, password, is_registered, overdue_charge, is_registered, rented_item_list);
+
+		super(id, name, email, password, can_borrow, overdue_charge, is_registered, rented_item_list);
+
 	}
 
 }
