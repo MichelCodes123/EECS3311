14f4fcb400d3f107853867be475789fd3a4813c9
chrispsang <christinapsang@gmail.com>
2024-03-14 13:22:22 -0400

    refactor

--- src/models/LibraryItem/EnableItemCommand.java
+++ src/models/LibraryItem/EnableItemCommand.java
@@ -1,26 +1,30 @@
 package models.LibraryItem;
 
-import models.Items.PhysicalItems.PhysicalItem;
 
+import models.Items.PhysicalItems.PhysicalItem;
 import java.util.List;
 
 public class EnableItemCommand implements Command {
     private int itemId;
+    private String itemType;
     private LibraryManager libraryManager;
 
-    public EnableItemCommand(int itemId, LibraryManager libraryManager) {
+    public EnableItemCommand(int itemId, String itemType, LibraryManager libraryManager) {
         this.itemId = itemId;
+        this.itemType = itemType;
         this.libraryManager = libraryManager;
     }
 
     @Override
     public void execute(List<PhysicalItem> items, String path, LibraryManager libraryManager) {
-        for (PhysicalItem item : items) {
-            if (item.getId() == itemId) {
-                item.setEnabled(true);
-                break;
-            }
-        }
-        libraryManager.updateLibrary(items, path);
+        libraryManager.enableItem(itemId, itemType);
     }
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
 }
