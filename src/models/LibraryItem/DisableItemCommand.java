2979b20da1f4321063439acd7b33566ded5c28d6
chrispsang <christinapsang@gmail.com>
2024-03-14 13:20:00 -0400

    refactor

--- src/models/LibraryItem/DisableItemCommand.java
+++ src/models/LibraryItem/DisableItemCommand.java
@@ -6,21 +6,25 @@ import java.util.List;
 
 public class DisableItemCommand implements Command {
     private int itemId;
+    private String itemType;
     private LibraryManager libraryManager;
 
-
-    public DisableItemCommand(int itemId, LibraryManager libraryManager) {
+    public DisableItemCommand(int itemId, String itemType, LibraryManager libraryManager) {
         this.itemId = itemId;
+        this.itemType = itemType;
         this.libraryManager = libraryManager;
     }
+
     @Override
     public void execute(List<PhysicalItem> items, String path, LibraryManager libraryManager) {
-        for (PhysicalItem item : items) {
-            if (item.getId() == itemId) {
-                item.setEnabled(false);
-                break;
-            }
-        }
-       libraryManager.updateLibrary(items, path);
+        libraryManager.disableItem(itemId, itemType);
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getItemType() {
+        return itemType;
     }
 }
