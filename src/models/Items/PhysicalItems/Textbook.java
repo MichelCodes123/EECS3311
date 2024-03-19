3fe6eff4c16aefde4d87dc3f1abadb9f8044a311
chrispsang <christinapsang@gmail.com>
2024-03-19 17:04:49 -0400

    modified using systemprompts

--- src/models/Items/PhysicalItems/Textbook.java
+++ src/models/Items/PhysicalItems/Textbook.java
@@ -1,22 +1,36 @@
 package models.Items.PhysicalItems;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.List;
 
-import models.FacultyObserver.FacultyObserver;
+import Prompts.SystemNotifications;
+import Prompts.SystemPrompts;
 import models.LibraryManagementTeam.LibraryManagementTeam;
 import models.LibraryManagementTeam.LibraryManagementTeamObserver;
+import models.TextbookObserver.TextbookObserver;
 
 public class Textbook extends Book {
-    private List<FacultyObserver> facultyObservers;
+    private List<TextbookObserver> facultyObservers;
     private List<LibraryManagementTeamObserver> libraryObservers;
     private boolean available;
+    private String lastNotification;
 
     public Textbook(String id, String name, String location, Boolean can_purchase, Long due_date, Double dollar_amount) {
         super(id, name, location, can_purchase, due_date, dollar_amount);
+        this.facultyObservers = new ArrayList<>();
+        this.libraryObservers = new ArrayList<>();
+        this.available = true; // Assume the textbook is initially available
+    }
+    
+    public List<TextbookObserver> getFacultyObservers() {
+        return facultyObservers;
     }
 
-    public void addObserver(FacultyObserver facultyObserver) {
+    public List<LibraryManagementTeamObserver> getLibraryObservers() {
+        return libraryObservers;
+    }
+
+    public void addObserver(TextbookObserver facultyObserver) {
         facultyObservers.add(facultyObserver);
     }
 
@@ -27,26 +41,22 @@ public class Textbook extends Book {
     public void setAvailability(boolean available) {
         this.available = available;
         if (available) {
-            notifyFacultyObservers();
+        	lastNotification = "New edition of textbook '" + getName() + "' is available.";
         } else {
-            notifyLibraryObservers();
+        	 lastNotification = "Textbook '" + getName() + "' is not available.";
         }
+        notifyObservers(new SystemNotifications(lastNotification));
     }
 
     public boolean isAvailable() {
         return available;
     }
-
-    private void notifyFacultyObservers() {
-        for (FacultyObserver observer : facultyObservers) {
-            observer.update(this);
+    private void notifyObservers(SystemPrompts prompt) {
+        for (TextbookObserver observer : facultyObservers) {
+            observer.update(prompt);
         }
-    }
-
-    private void notifyLibraryObservers() {
         for (LibraryManagementTeamObserver observer : libraryObservers) {
-            observer.update(this);
+            observer.update(prompt);
         }
     }
-
 }
