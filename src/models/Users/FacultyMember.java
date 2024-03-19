5c231fe4fc20fb6b378f521378be524f1682e4a8
chrispsang <christinapsang@gmail.com>
2024-03-19 17:08:10 -0400

    refactor using systemprompts

--- src/models/Users/FacultyMember.java
+++ src/models/Users/FacultyMember.java
@@ -4,19 +4,18 @@ package models.Users;
 import java.util.ArrayList;
 import java.util.List;
 
+import Prompts.SystemPrompts;
 import models.Course;
-import models.FacultyObserver.FacultyObserver;
-import models.Items.Item;
-import models.Items.PhysicalItems.Book;
 import models.Items.PhysicalItems.Textbook;
+import models.TextbookObserver.TextbookObserver;
 
-public class FacultyMember extends User implements FacultyObserver {
+public class FacultyMember extends User implements TextbookObserver {
 	private List<Course> teachingCourses;
     private List<Textbook> previousTextbooksUsed;
-    private String lastNotification;
+	private String lastNotification;
 
-	public FacultyMember(String id, String name, String email, String password, Boolean can_borrow,
-			Double overdue_charge, Boolean is_registered, ArrayList<String> rented_item_list) {
+
+	public FacultyMember(String id, String name, String email, String password, Boolean can_borrow, Double overdue_charge, Boolean is_registered, ArrayList<String> rented_item_list) {
 		super(id, name, email, password, can_borrow, overdue_charge, is_registered, rented_item_list);
 		this.teachingCourses = new ArrayList<>();
         this.previousTextbooksUsed = new ArrayList<>();
@@ -35,12 +34,12 @@ public class FacultyMember extends User implements FacultyObserver {
     public void addTextbookUsed(Textbook textbook) {
         previousTextbooksUsed.add(textbook);
     }
-    public String getLastNotification() {
+    public String update(SystemPrompts prompt) {
+        lastNotification = "Faculty Member Notification: " + prompt.getMessage();
         return lastNotification;
     }
-
-    public String update(Textbook textbook) {
-    	 lastNotification = "Faculty Member Notification: New edition of textbook '" + textbook.getName() + "' is available.";
-         return lastNotification;
-    }
+    
+    public String getLastNotification() {
+		return lastNotification;    
+	}
 }
