b7e63c37e30f2b843364864871cff99dab4e7d30
Philly <lfilippelli5@gmail.com>
2024-04-01 18:13:32 -0400

    PaymentService tests

--- src/Tests/PaymentTest.java
+++ src/Tests/PaymentTest.java
@@ -1,11 +1,16 @@
 package Tests;
 
 import database_access.FacultyMemberAccess;
+import models.Payments.PaymentOption;
 import models.Users.FacultyMember;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import services.Payments.PaymentService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
 public class PaymentTest {
@@ -16,20 +21,59 @@ public class PaymentTest {
 
 
         FacultyMember prof = new FacultyMember("id","Name", "location", "password",  true, 1.0, true, new ArrayList<>(), new ArrayList<>() );
-        FacultyMemberAccess profdb = FacultyMemberAccess.getInstance();
-        profdb.users.add(prof);
-        profdb.update();
+        FacultyMemberAccess userdb = FacultyMemberAccess.getInstance();
+        userdb.users.add(prof);
+        userdb.update();
 
         service.overduePay(1.0, "Credit", prof);
         assertEquals(0.0, prof.getOverdue_charge());
 
-        profdb.load();
-        profdb.users.set(0, prof);
-        profdb.update();
-        profdb.load();
-        assertEquals(0.0, profdb.users.get(0).getOverdue_charge());
+        userdb.load();
+        userdb.users.set(0, prof);
+        userdb.update();
+        userdb.load();
+        assertEquals(0.0, userdb.users.get(0).getOverdue_charge());
 
-        profdb.users = new ArrayList<>();
-        profdb.update();
+        userdb.users = new ArrayList<>();
+        userdb.update();
+    }
+
+    @Test
+    void discountPay() throws Exception {
+        PaymentService service = new PaymentService();
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        PrintStream old_output = System.out;
+        System.setOut(new PrintStream(output));
+
+        FacultyMember prof = new FacultyMember("id","Name", "location", "password",  true, 1.0, true, new ArrayList<>(), new ArrayList<>() );
+        FacultyMemberAccess userdb = FacultyMemberAccess.getInstance();
+        userdb.users.add(prof);
+        userdb.update();
+
+        service.discountPay(prof.getName(), 1.0, "Credit");
+        assertEquals("Youve paid", output.toString());
+
+        System.setOut(old_output);
+    }
+
+    @Test
+    void pOptionCredit() {
+        PaymentService service = new PaymentService();
+
+        assertDoesNotThrow(() ->  service.pOption("Credit", 2.0));
+    }
+
+    @Test
+    void pOptionDebit() {
+        PaymentService service = new PaymentService();
+
+        assertDoesNotThrow(() ->  service.pOption("Debit", 2.0));
+    }
+
+    @Test
+    void pOptionMobileWallet() {
+        PaymentService service = new PaymentService();
+
+        assertDoesNotThrow(() ->  service.pOption("Mobile Wallet", 2.0));
     }
 }
