42ca92789b3afd55163bf0769dcdb4c4a5344c1c
Philly <lfilippelli5@gmail.com>
2024-04-02 14:25:06 -0400

    Request book

--- src/Tests/RequestBookTest.java
+++ src/Tests/RequestBookTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeAll;
 
 import models.Request;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import services.RequestBook;
 import services.RequestService;
@@ -39,6 +40,42 @@ class RequestBookTest {
 		Request r = rs.requestQueue.peek();
 		assertEquals("Course", r.getType());
 
+	}
+
+	@Test
+	@DisplayName("Anything other than Course gets low priority")
+	void testLowPriority() {
+		RequestBook.execute("120-11", "1", "Turtles");
+		RequestBook.execute("120-12", "2", "Course");
+
+		RequestService rs = RequestService.getInstance();
+
+		Request r = rs.requestQueue.peek();
+		assertEquals("Course", r.getType());
+	}
+
+	@Test
+	@DisplayName("Oldest entry takes precedence for high priority")
+	void testPriorityOrdering() {
+		RequestBook.execute("120-11", "1", "Course");
+		RequestBook.execute("120-12", "2", "Course");
+
+		RequestService rs = RequestService.getInstance();
+
+		Request r = rs.requestQueue.peek();
+		assertEquals("120-11", r.getBookId());
+	}
+
+	@Test
+	@DisplayName("Oldest entry takes precedence for low priority")
+	void testPriorityOrdering1() {
+		RequestBook.execute("120-11", "1", "Blah");
+		RequestBook.execute("120-12", "2", "Blah");
+
+		RequestService rs = RequestService.getInstance();
+
+		Request r = rs.requestQueue.peek();
+		assertEquals("120-11", r.getBookId());
 	}
 
 }
