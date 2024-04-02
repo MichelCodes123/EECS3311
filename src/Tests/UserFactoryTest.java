398840139a34c4b613341a6ea4f811880e7fb6d7
MichelCodes123 <97976216+MichelCodes123@users.noreply.github.com>
2024-04-02 13:56:26 -0400

    Update Usr factory test

--- src/Tests/UserFactoryTest.java
+++ src/Tests/UserFactoryTest.java
@@ -1,28 +1,33 @@
 package Tests;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import logic.UserFactory;
+import models.Users.Student;
+import models.Users.User;
 
 class UserFactoryTest {
 
-	UserFactory u; 
+	public static UserFactory u; 
 	
 	@BeforeAll
-	void init() {
+	static void init() {
 		u = new UserFactory();
 	}
 
 	@Test
 	void verifyStudentCreated() {
-		
-		UserFactory u = new UserFactory();
-		
+		User usr = u.CreateUser("Student", "1011", "Johnny", "j@gmail.com", "pass", true,0.0, true, null, null);
+	
+		assertTrue(usr instanceof Student);
 	}
 	@Test
 	void verifyFacultyCreated() {
 		
+		User usr = u.CreateUser("Student", "1011", "Johnny", "j@gmail.com", "pass", true,0.0, true, null, null);
 	}
 	@Test
 	void verifyVisitorCreated() {
